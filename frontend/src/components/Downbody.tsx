fde304a2b922a287de4686de023045deedbb0b39
frankkdesmondd <frankkdesmondd@gmail.com>
2025-12-20 14:56:25 +0100

    Update Downbody.tsx

--- frontend/src/components/Downbody.tsx
+++ frontend/src/components/Downbody.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Downbody: React.FC = () => {
   return (
-    <div className="relative z-10 mt-[-5em] md:mt-[-24em] lg:mt-[-25em] px-4 sm:px-6 md:px-8 lg:px-16 xl:px-20 items-center mb-16 sm:mb-24 md:mb-32 lg:mb-[55em]">
+    <div className="relative z-10 mt-[-5em] md:mt-[-24em] lg:mt-[-25em] px-4 sm:px-6 md:px-8 lg:px-16 xl:px-20 items-center mb-[5em]">
       
-      <div className="relative lg:absolute bg-white w-full lg:w-[90%] xl:w-[88%] flex flex-col justify-center items-center py-6 sm:py-8 md:py-12 lg:py-14 xl:py-[3.6em] left-1/2 -translate-x-1/2 px-4 sm:px-6 md:px-8 lg:px-12 xl:px-[5em] gap-4 sm:gap-6 md:gap-8 lg:gap-[1.4em] shadow-lg md:shadow-xl">
+      <div className="relative bg-white w-full flex flex-col justify-center items-center py-6 sm:py-8 md:py-12 lg:py-14 xl:py-[3.6em] px-4 sm:px-6 md:px-8 lg:px-12 xl:px-[5em] gap-4 sm:gap-6 md:gap-8 lg:gap-[1.4em] shadow-lg md:shadow-xl">
         
         {/* Header Section */}
         <div className="text-center w-full">
@@ -64,7 +64,8 @@ const Downbody: React.FC = () => {
               </p>
               <div className='flex w-full h-[0.03em] bg-gray-300'></div>
               <p className='text-[#244772] text-base sm:text-lg md:text-xl leading-relaxed'>
-                By developing proven areas with highly reputable companies and employing state-of-the-art drilling technologies, oil and gas risk is minimized.
+                By developing proven areas with highly reputable companies and employing state
+                of-the-art drilling technologies, oil and gas risk is minimized.
               </p>
             </div>
           </div>
@@ -74,6 +75,4 @@ const Downbody: React.FC = () => {
   )
 }
 
-
 export default Downbody
-
