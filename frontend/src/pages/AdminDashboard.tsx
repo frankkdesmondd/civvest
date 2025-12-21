1ab7ba56952adaa7c6f1e4473171450798049419
frankkdesmondd <frankkdesmondd@gmail.com>
2025-12-21 14:59:37 +0100

    Update AdminDashboard.tsx

--- frontend/src/pages/AdminDashboard.tsx
+++ frontend/src/pages/AdminDashboard.tsx
@@ -1464,20 +1464,22 @@ const resetEditModal = () => {
                               {post.published ? 'Published' : 'Draft'}
                             </span>
                           </td>
-                          <div className="flex gap-2">
-    <button 
-      onClick={() => handleEditNews(post)} 
-      className="text-blue-600 hover:text-blue-800"
-    >
-      <FiEdit />
-    </button>
-    <button 
-      onClick={() => handleDeleteNews(post.id)} 
-      className="text-red-600 hover:text-red-800"
-    >
-      <FiTrash2 />
-    </button>
-  </div>
+                          <td className="py-4 px-4"> {/* This td was missing! */}
+                            <div className="flex gap-2">
+                              <button 
+                                onClick={() => handleEditNews(post)} 
+                                className="text-blue-600 hover:text-blue-800"
+                              >
+                                <FiEdit />
+                              </button>
+                              <button 
+                                onClick={() => handleDeleteNews(post.id)} 
+                                className="text-red-600 hover:text-red-800"
+                              >
+                                <FiTrash2 />
+                              </button>
+                            </div>
+                          </td> {/* Closing td tag */}
                         </tr>
                       ))}
                     </tbody>
@@ -1832,3 +1834,4 @@ const resetEditModal = () => {
 };
 
 export default AdminDashboard;
+
