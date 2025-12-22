df783461e5fde160b8e0ce19085d77376e9d7df3
frankkdesmondd <frankkdesmondd@gmail.com>
2025-12-22 07:48:28 +0100

    Update InvestHeroSection.tsx

--- frontend/src/components/InvestHeroSection.tsx
+++ frontend/src/components/InvestHeroSection.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import InvestHeroImage from '../assets/refinery.jpg'
 import { Link } from 'react-router-dom'
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const InvestHeroSection: React.FC = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,     // animation duration
+      easing: "ease-in-out",
+      once: true,         // animation runs once
+      offset: 100,        // trigger distance
+    });
+
+    // Refresh AOS when DOM changes
+    AOS.refresh();
+  }, []);
+  
+
   return (
-    <div  className="relative w-full h-[45em] flex items-center justify-center overflow-hidden">
+    <div  className="relative w-full h-[45em] flex items-center justify-center overflow-hidden" data-aos="fade-up">
       <img
         className="absolute top-0 left-0 w-full h-full object-cover mt-[10em] -z-10"
         src={InvestHeroImage} alt=''
@@ -13,7 +28,7 @@ const InvestHeroSection: React.FC = () => {
        {/* Dark Blur Overlay */}
       <div className="absolute top-0 left-0 w-full h-full bg-black/70 backdrop-blur-[2px] pointer-events-none -z-5"></div>
 
-      <div className='flex flex-col relative z-30 text-start text-white items-start px-[2em] mt-[10em] lg:mt-[10em] w-full gap-[2em]'>
+      <div className='flex flex-col relative z-30 text-start text-white items-start px-[2em] mt-[10em] lg:mt-[10em] w-full gap-[2em]' data-aos="fade-up">
         <p className='flex text-[2.5em] lg:text-[4.4em]'>Invest In Civvest Energy Partners</p>
         <div className='flex bg-blue-300 h-[0.2em] w-full'></div>
         <p className='flex flex-wrap lg:max-w-[25em] text-[1.5em] font-light'>Freedom means investing on your terms. Civvest Energy Partners bond offerings are now open to investors.*</p>
